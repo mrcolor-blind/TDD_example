1e092e567c4a6b5ba4ffc81d38e5b9c9c2a4225f
miguelangel <mikebarrientosb@gmail.com>
2025-03-31 07:28:09 -0600

    Spring Initialized

---
+++ src/test/java/com/example/TDD_Example/TddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TDD_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
